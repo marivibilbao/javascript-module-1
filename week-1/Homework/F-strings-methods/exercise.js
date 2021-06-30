e276112bc2aff9bba4db0dfc348dd8c4e58aef6c
marivibilbao <marivibilbaog@gmail.com>
2021-06-30 23:17:03 +0200

    Agrego ejercicio F-1

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
+function frase(name){
+    var nameLength = name.length;
+    let mensaje = "My name is " + name + " and my name is " + nameLength + " characters long.";
+    return mensaje;
+}
 
+let message = frase ("María");
 console.log(message);
