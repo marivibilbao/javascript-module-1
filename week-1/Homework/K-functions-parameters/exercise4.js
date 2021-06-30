45bf00515f0e319f24ed836da73961f2c0c9be71
marivibilbao <marivibilbaog@gmail.com>
2021-06-30 23:58:14 +0200

    Agrego ejercicio k-4

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,10 @@
 // Declare your function first
-
+    function sum1(a, b){
+        return a + b;
+    }
 // Call the function and assign to a variable `sum`
+
+// Llamo a la función sum1 y le asigno los valores que dice el ejercio.
+let sum = sum1(13, 124);
 
 console.log(sum);
