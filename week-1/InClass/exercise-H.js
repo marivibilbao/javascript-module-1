15a1e1de4e6ee2cbfed964a9ffc84331eca1c9bc
marivibilbao <marivibilbaog@gmail.com>
2021-06-30 23:02:34 +0200

    Agrego modificación al ejercicio H

--- week-1/InClass/exercise-H.js
+++ week-1/InClass/exercise-H.js
@@ -1,9 +1,17 @@
-function operaciones(nombre , edad) {
-    let anyo = 2021;
-    const anyoNacimiento = anyo - edad;
-    const mensaje = "Hola mi nombre es " + nombre + " y nací en el año " + anyoNacimiento;
-    return mensaje;
+function sum(a, b) {
+    let suma = a+b;
+   return suma;
 }
 
-let saludo = operaciones ("María" , 32);
-console.log(saludo);
+console.log("Suma: " + sum(2,5)); 
+
+// Este fue el ejercicio que hice durante la clase.
+//function operaciones(nombre , edad) {
+    //let anyo = 2021;
+    //const anyoNacimiento = anyo - edad;
+    //const mensaje = "Hola mi nombre es " + nombre + " y nací en el año " + anyoNacimiento;
+    //return mensaje;
+//}
+
+//let saludo = operaciones ("María" , 32);
+//console.log(saludo);
