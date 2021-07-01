b26a69617e15cb1a35ca0ed8f8d75fe5f21ee11e
marivibilbao <marivibilbaog@gmail.com>
2021-07-01 22:26:55 +0200

    Modifico ejercicio L-2, ya que no estaba resuelto

--- week-1/Homework/L-functions-nested/exercise2.js
+++ week-1/Homework/L-functions-nested/exercise2.js
@@ -1,23 +1,26 @@
+//Declaración de variables
+var mentor1 = "Daniel";
+var mentor2 = "Irina";
+var mentor3 = "Mimi";
+var mentor4 = "Rob";
+var mentor5 = "Yohannes";
+
 // Función para convertir letras en mayúsculas:
-
 function mentors(names){
-    return mayu = ment.toUpperCase();
+    return mayu = names.toUpperCase();
 }
 
-function message(mensaje){
-    let mensaje = "Hello";
-    return mensaje;
+//Función que imprime el mensaje + nombre en mayúsculas y llama a la función "mentors"
+function message(name1, name2, name3, name4, name5){
+    console.log("HELLO " + mentors(name1)); //Imprime en pantalla 4 veces
+    console.log("HELLO " + mentors(name2));
+    console.log("HELLO " + mentors(name3));
+    console.log("HELLO " + mentors(name4));
+    console.log("HELLO " + mentors(name5));
 }
 
-let varios = message (mensaje , "Daniel");
-console.log(varios);
-
-
-//var mentor1 = "Daniel";
-//var mentor2 = "Irina";
-//var mentor3 = "Mimi";
-//var mentor4 = "Rob";
-//var mentor5 = "Yohannes";
+//Llama a la función mensaje con los parámetros mentor.
+message(mentor1, mentor2, mentor3, mentor4, mentor5);
 
 
 //JavaScript existe la función toLowerCase() para convertir a «minúsculas», 
