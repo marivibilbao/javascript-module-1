272a6490e9101ae1bb1298cb853f59ed3d0d1e9f
marivibilbao <marivibilbaog@gmail.com>
2021-07-01 18:56:02 +0200

    Agrego cambios en ejercicio F-2

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,8 +1,8 @@
 function frase(name){
-    var nameLength = name.length;
+    var nameLength = name.trim().length; // Agrego .trim() para que quite espacios en el nombre.
     let mensaje = "My name is " + name + " and my name is " + nameLength + " characters long.";
     return mensaje;
 }
 
-let message = frase ("María");
-console.log(message.trim());
+let message = frase ("María "); // Agrego espacio para que la función .trim() quite espacios.
+console.log(message);
