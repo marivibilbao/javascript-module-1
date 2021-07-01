8047cfdb67c03aa042b640bff2a427200e37584e
marivibilbao <marivibilbaog@gmail.com>
2021-07-01 19:37:48 +0200

    Agrego ejercicio L-1 con dos funciones, pero debo revisarlo

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,27 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+function calcStude (numberOfStudents, numberOfMentors){
+    let total = numberOfStudents + numberOfMentors;
+    let porcentajeStudents = (numberOfStudents / total)*100;
+    let message1 = ("Percentage students: " + Math.round(porcentajeStudents) + "%");
+    return message1;
+}
+
+function calcMent (numberOfStudents, numberOfMentors){
+    let total = numberOfStudents + numberOfMentors;
+    let porcentajeMentors = (numberOfMentors / total)*100;
+    let message2 = ("Percentage mentors: " + Math.round(porcentajeMentors) + "%");
+    return message2;
+}
+
+let mm1 = calcStude(15, 8);
+console.log(mm1);
+
+let mm2 = calcMent(15, 8);
+console.log(mm2);
+
+
+//Información de entrada que va en el ejercicio 2 (Error L-2)
+//var mentor1 = "Daniel";
+//var mentor2 = "Irina";
+//var mentor3 = "Mimi";
+//var mentor4 = "Rob";
+//var mentor5 = "Yohannes";
---
+++ week-1/Homework/L-functions-nested/exercise2.js
@@ -0,0 +1,33 @@
+// Función para convertir letras en mayúsculas:
+
+function mentors(names){
+    return mayu = ment.toUpperCase();
+}
+
+function message(mensaje){
+    let mensaje = "Hello";
+    return mensaje;
+}
+
+let varios = message (mensaje , "Daniel");
+console.log(varios);
+
+
+//var mentor1 = "Daniel";
+//var mentor2 = "Irina";
+//var mentor3 = "Mimi";
+//var mentor4 = "Rob";
+//var mentor5 = "Yohannes";
+
+
+//JavaScript existe la función toLowerCase() para convertir a «minúsculas», 
+//y la función toUpperCase() para convertir a «mayúsculas», según sea el caso.
+
+//Ejemplo A: Convertir cadena de texto a Mayúscula
+//--cadena = 'Texto De Prueba';
+//--cadena = cadena.toUpperCase();
+//--document.write(cadena);
+
+//Ejemplo B:  Convertir texto de minúsculas a mayúsculas usando prompt
+//--var texto = prompt("Introduce el texto");
+//--document.write(texto.toUpperCase());// Imprimira en mayuscula
