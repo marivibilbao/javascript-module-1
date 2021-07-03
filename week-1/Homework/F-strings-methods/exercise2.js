07ed3e28b749f819cccf4afb3d83e4936c854b36
marivibilbao <marivibilbaog@gmail.com>
2021-07-03 13:51:35 +0200

    Agrego otra forma de hacer el ejercicio 2-F

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,8 +1,14 @@
-function frase(name){
-    var nameLength = name.trim().length; // Agrego .trim() para que quite espacios en el nombre.
-    let mensaje = "My name is " + name + " and my name is " + nameLength + " characters long.";
-    return mensaje;
-}
+let name = "María ";
+let message = "My name is " + name + " and my name is " + name.trim().length + " characters long.";
 
-let message = frase ("María "); // Agrego espacio para que la función .trim() quite espacios.
 console.log(message);
+
+//Lo hice como función y lo pedían sin función
+//function frase(name){
+//    var nameLength = name.trim().length; // Agrego .trim() para que quite espacios en el nombre.
+//    let mensaje = "My name is " + name + " and my name is " + nameLength + " characters long.";
+//    return mensaje;
+//}
+
+//let message = frase ("María "); // Agrego espacio para que la función .trim() quite espacios.
+//console.log(message);
