dd4bdb6693746ab3f49c94e9003f7c36fae7628c
marivibilbao <marivibilbaog@gmail.com>
2021-07-03 13:48:58 +0200

    Agrego otra forma de hacer el ejercicio 1-F

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,10 +1,15 @@
 //Start by creating a variable `message`
 
-function frase(name){
-    var nameLength = name.length; // .length cuenta la cantidad de letras que tiene mi nombre
-    let mensaje = "My name is " + name + " and my name is " + nameLength + " characters long.";
-    return mensaje;
-}
-
-let message = frase ("María");
+let name = "María";
+let message = "My name is " + name + " and my name is " + name.length + " characters long.";
 console.log(message);
+
+//Lo hice como función y lo pedían sin función
+//function frase(name){
+//    var nameLength = name.length; // .length cuenta la cantidad de letras que tiene mi nombre
+//    let mensaje = "My name is " + name + " and my name is " + nameLength + " characters long.";
+//    return mensaje;
+//}
+
+//let message = frase ("María");
+//console.log(message);
