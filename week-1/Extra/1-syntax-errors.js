fb43df0a9d7143edfe7f31f58353bf43375ed046
marivibilbao <marivibilbaog@gmail.com>
2021-07-03 11:51:15 +0200

    Agrego ejercicio 1 realizado en clase

--- week-1/Extra/1-syntax-errors.js
+++ week-1/Extra/1-syntax-errors.js
@@ -1,17 +1,18 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
-function addNumbers(a b c) {
+function addNumbers(a, b, c) { //Agrego comas en cada parámetro
     return a + b + c;
 }
 
-function introduceMe(name, age)
-return "Hello, my name is " + name "and I am " age + "years old";
+function introduceMe(name, age) { //Agrego corchete de inicio y fin de la función
+    return "Hello, my name is " + name + " and I am " + age + " years old"; //Agrego + antes y después del "and I am. Agrego espacios faltantes"
+}
 
 function getTotal(a, b) {
-    total = a ++ b;
+    total = a + b; //Elimino un + ya que estaba doble
 
     // Use string interpolation here
-    return "The total is %{total}"
+    return `The total is ${total}`;
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
