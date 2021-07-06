c18842c4fe37852d2ff4746584b02da7e7d0bafd
marivibilbao <marivibilbaog@gmail.com>
2021-07-06 20:55:21 +0200

    Agrego ejercicio G realizado en clase

--- week-2/InClass/exercise-G.js
+++ week-2/InClass/exercise-G.js
@@ -1,4 +1,8 @@
 var apolloCountdownMessage = "all engine running... LIFT-OFF!";
 var countdown = 8;
 
+while (countdown>=0){
+    console.log(countdown);
+    countdown -=1; //Se puede colocar -- (sin el 1) que seria es igual a 1
+}
 console.log(apolloCountdownMessage);
