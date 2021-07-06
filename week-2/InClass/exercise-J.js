edae5e013758aba88aab8a2d7183d70c1e133350
marivibilbao <marivibilbaog@gmail.com>
2021-07-06 21:44:17 +0200

    Agrego ejercicio J en clase

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,8 @@
 function secondMatchesAmy(array) {
-  if ( ) {
+  if (array[1]==="Amy") {
     return "Second index matched!";
   }
   return "Second index not matched";
 }
+
+console.log(secondMatchesAmy(["uno", "dos", "tres"]));
