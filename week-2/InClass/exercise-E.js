6e1c2f86eaabb642f07b5944b027bc2bd447a55e
marivibilbao <marivibilbaog@gmail.com>
2021-07-06 20:31:13 +0200

    Realizamos ejercicio en clase E

---
+++ week-2/InClass/exercise-E.js
@@ -0,0 +1,10 @@
+//Utilizamos consola node
+
+//let num = 10 ---- undefined
+//num > 5 && num < 15 ----- true
+//num > 10 || num === 10 --- true
+//false || true ------ true
+///NOTtrue ------ false
+//let greaterThan5 = num > 5 --------- undefined
+//NOTgreaterThan5 -------- false
+//NOY(num === 10) ----------- false
