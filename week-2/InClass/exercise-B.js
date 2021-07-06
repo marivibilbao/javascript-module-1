4543c2149983d074999bba5e373d2d1e030b93ea
marivibilbao <marivibilbaog@gmail.com>
2021-07-06 19:13:30 +0200

    Se modifica ejercicio en clase

--- week-2/InClass/exercise-B.js
+++ week-2/InClass/exercise-B.js
@@ -1,9 +1,9 @@
 function boolChecker(bool) {
-  if (typeof bool === ) {
+  if (typeof bool === "boolean") { //Se completa con boolean
     return "You've given me a bool, thanks!";
   }
 
-  return "No bool, not cool.";
+  return "No bool, not cool."; //No se ejecuta porque es el segundo de la función y sólo se ejecuta uno.
 }
 
-boolChecker(true);
+console.log(boolChecker(true)); //Agregamos un console.log
