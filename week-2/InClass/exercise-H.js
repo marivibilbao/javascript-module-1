47b23d9c5f2bb9433bc965f0fd9e56a3b3a89c04
marivibilbao <marivibilbaog@gmail.com>
2021-07-06 21:27:05 +0200

    Agrego ejercicio H realizado en clase

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -5,3 +5,9 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+for( let num=5; num<=20; num++){
+  if(isEven(num)){
+    console.log("El exponencial de " + num + " es " + exponential(num));
+  }
+}
