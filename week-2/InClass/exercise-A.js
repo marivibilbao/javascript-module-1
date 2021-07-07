fbfce92953b36dd2950413f772d99a7afcb093dd
marivibilbao <marivibilbaog@gmail.com>
2021-07-07 10:44:19 +0200

    Repito ejercicio y agrego comentarios al archivo

---
+++ week-2/InClass/exercise-A.js
@@ -0,0 +1,10 @@
+//Con el terminar en node, realizamos el ejercicio.
+//Ejercicio ----- Resultado
+//1+2 ----------- 3
+//"hello" ------- 'hello'
+//let favouriteColour="purple" ----- undefined (Estoy introduciendo una variable)
+//favouriteColour ---------- 'purple'
+//console.log(favouriteColour) ----------   purple
+//                             ----------   undefined
+
+// Para salir de la consola de node se debe hacer dos veces Ctrl + c.
