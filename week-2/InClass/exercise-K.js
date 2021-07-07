c290da33a7687e7848423f3ad6d1ca47dc6a1541
marivibilbao <marivibilbaog@gmail.com>
2021-07-07 16:18:46 +0200

    Agrego ejercicio K-InClass

---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,9 @@
+function printStudents(students){
+    for (let i=0; i<students.length; i++) {
+        console.log(students[i]);
+    }
+}
+
+let students = ["Nancy", "Pedro", "Jesús", "María Victoria"];
+
+printStudents(students);
