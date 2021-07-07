c2e5921396e47d9fe08d5105bc275dee523525d7
marivibilbao <marivibilbaog@gmail.com>
2021-07-07 15:54:49 +0200

    Agrego comentarios del ejercicio I

---
+++ week-2/InClass/exercise-I.js
@@ -0,0 +1,12 @@
+//Ejercicio en node REPL
+
+//Ejercicio ------ Respuesta
+//const fruits = ['banana', 'apple', 'strawberry', 'kiwi', 'fig', 'orange']; --------- undefined
+//fruits [2] ---- 'strawberry'
+//fruits [3] ---- 'kiwi'
+//fruits [5] ---- 'orange'
+//fruits [0] ---- 'banana'
+
+//Reemplazar 'apple' con 'raspberry', y reemplaza 'fig' con 'pineapple'.
+//fruits [1] = "raspberry"; ------ 'raspberry'
+//fruits [4] = "pineapple"; ------ 'pineapple'
