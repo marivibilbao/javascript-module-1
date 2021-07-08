7e02585ed4aafa8f17f4806e74f5ae71f00c6124
marivibilbao <marivibilbaog@gmail.com>
2021-07-08 16:30:50 +0200

    Se modifica ejercicio según diapositivas

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,8 +1,14 @@
 function secondMatchesAmy(array) {
-  if (array[1]==="Amy") {
+  if (array[1] === "Amy") {
     return "Second index matched!";
   }
   return "Second index not matched";
 }
 
-console.log(secondMatchesAmy(["uno", "dos", "tres"]));
+let names = ["Alex", "Amara", "Carlos"];
+let names2 = ["Anna", "Amy", "Natalia"];
+
+const result = secondMatchesAmy(names2); 
+console.log(result);
+
+//console.log(secondMatchesAmy(["uno", "dos", "tres"]));
