a26d7a82c6b3dac867a6dd948c1a0f2df3f8cd78
marivibilbao <marivibilbaog@gmail.com>
2021-07-08 21:23:10 +0200

    Agrego ejercicio 1-M-Homework

--- week-2/Homework/M-array-loop/exercise.js
+++ week-2/Homework/M-array-loop/exercise.js
@@ -3,6 +3,17 @@
 	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
 */
 
+function toGetStringT(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    //Declaro variable i = longitud del arreglo -1 da la posición de los datos
+    //Condición i > 0 para que se ejecute el for siempre que se cumpla /
+    //Descuento 1 a valor de i.
+    if (array[i][0] === "T") { // 0 Es el primer caracter de la palabra.
+      //Declaro un condicional, El primer caracter comienza con T mayúscula, si es si impreme en pantalla el valor del dato en esa posición.
+      console.log(array[i]);
+    }
+  }
+}
 
 const daysOfWeek = [
   "Monday",
@@ -13,3 +24,5 @@ const daysOfWeek = [
   "Saturday",
   "Sunday",
 ];
+
+toGetStringT(daysOfWeek);
