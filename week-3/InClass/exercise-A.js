07bc7e5c66d15c9a474e41bb1de68c2ede5597f6
marivibilbao <marivibilbaog@gmail.com>
2021-07-14 10:38:09 +0200

    Agrego ejercicio A-InClass-Week3

---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,7 @@
+let names = ["Jesús", "Nancy", "Pedro", "María Victoria"];
+
+let nameInicio = names.unshift ("Sasha"); //Agregar nombre al inicio del array utilizando .unshift
+names[5] = "Ruvi"; //Agrego al final de la lista un nombre.
+
+console.log(names); //Imprimo nombres del array.
+console.log(names.length); //LENGTH para saber la cantidad de string que tiene el array.
