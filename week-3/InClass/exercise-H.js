40f10ef84ed8752d5476654a031e484bb26219f0
marivibilbao <marivibilbaog@gmail.com>
2021-07-14 12:14:46 +0200

    Agrego ejercicio H-InClass-Week3

---
+++ week-3/InClass/exercise-H.js
@@ -0,0 +1,13 @@
+//Ejercicio - Create a function which:
+//Takes an array of names
+//Looks to see if your name is in the array
+//If it is, return Found me!; if not, return Haven't found me :(
+
+//------------------------------------------------------------------------
+
+let names = ["Jesús", "Nancy", "Pedro", "María Victoria"];
+let myName = names.find(names => names === "María Victoria") ? "Found me!" : "Haven't found me :(" 
+// Si mi nombre lo cambio por Ana me dice que no se encontró.
+// Utilizo una condición de la siguiente manera ---> ? val1 : val2
+
+console.log(myName);
