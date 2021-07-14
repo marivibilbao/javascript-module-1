c4141f897752a74f1832e4eff48a392570108d8c
marivibilbao <marivibilbaog@gmail.com>
2021-07-14 20:15:28 +0200

    Agrego información adicional al ejercicio

--- week-3/InClass/exercise-A.js
+++ week-3/InClass/exercise-A.js
@@ -6,4 +6,4 @@ let nameFinal = names.push("Ruvi"); //Agrego al final de la lista un nombre.
 //names[5] = "Ruvi"; //Cuando sabes la posición. Agrego al final de la lista un nombre.
 
 console.log(names); //Imprimo nombres del array.
-console.log(names.length); //LENGTH para saber la cantidad de string que tiene el array.
+console.log("Número de personas de la lista: " + names.length); //LENGTH para saber la cantidad de string que tiene el array.
