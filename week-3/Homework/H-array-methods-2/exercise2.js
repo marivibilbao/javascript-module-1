9a39b05599ca6c5816eaf6b6906002cc47601ea4
marivibilbao <marivibilbaog@gmail.com>
2021-07-15 22:41:36 +0200

    Agrego ejercicio 2-H-Homework-Week3

--- week-3/Homework/H-array-methods-2/exercise2.js
+++ week-3/Homework/H-array-methods-2/exercise2.js
@@ -7,7 +7,19 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  //return str.split(nombre => nombre.charAt(0).toUpperCase()).join();
+  let primeraMayusc = str[0].toUpperCase() + str.slice(1);
+  return primeraMayusc.split().join();
+}
+
+
+// SPLIT() Divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
+
+//JOIN() Une todos los elementos de una matriz (o un objeto similar a una matriz) en una cadena y devuelve esta cadena.
+
+//SLICE() Devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido). 
+//El array original no se modificará.
 
 /* 
   DO NOT EDIT BELOW THIS LINE
