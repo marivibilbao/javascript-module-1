be239370fc1311cdbf651adf8940fabbb1eec364
marivibilbao <marivibilbaog@gmail.com>
2021-07-17 13:56:25 +0200

    Agrego ejercicio 2-Extra-Week3

--- week-3/Extra/2-bush-berries.js
+++ week-3/Extra/2-bush-berries.js
@@ -10,8 +10,12 @@
   Use the tests to confirm which message to return
 */
 
-function bushChecker() {
-
+function bushChecker(bushColours) {
+  if (bushColours.every((colour) => colour === "pink")){
+    return "Bush is safe to eat from"
+  }else {
+    return "Toxic! Leave bush alone!"
+  }
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
